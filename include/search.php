73a43b057b7351085ea30cf676b291cd060189e2
sadak salaheddine <sadaksalaheddine@gmail.com>
2016-09-21 18:16:18 +0100

    First Version Of The Project

---
+++ include/search.php
@@ -0,0 +1,14 @@
+<section>
+    <div class="col-md-12 col-sm-12 searche-style-all">
+        <h1>You Search New <label>BUZZ !!</label></h1>
+        <div class="search-style form-group has-feedback">
+            <input type="text" class="form-control" id="inputSuccess2" placeholder="Comedie , Dramas , Films , Sport ......"/>
+            <button class="glyphicon glyphicon-search form-control-feedback"></button>
+        </div>
+    </div> 
+</section>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>Home </title>
+
+        <?php include './include/scripts_css.php'; ?>
+
+    </head><!--/head-->
+
+    <body>
+
+        <?php include './include/header.php'; ?>  
+        <?php include './include/search.php'; ?>  
+
+        <div class="container">
+            <?php include './include/left_menu.php'; ?>  
+            <?php include './include/home_container.php'; ?>  
+        </div>        
+
+
+
+<?php include './include/social.php'; ?>  
+<?php include './include/footer.php'; ?>  
+        <?php include './include/scripts_js.php'; ?> 
+    </body>
+</html>
